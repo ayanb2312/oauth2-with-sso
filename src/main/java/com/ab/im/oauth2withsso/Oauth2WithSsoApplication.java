18a531ec3c5ba3261e0d27adb87ce8f8844e68b0
Bose, Ayan <ayan.bose@siemens.com>
2020-11-13 22:26:56 +0530

    draft push

---
+++ src/main/java/com/ab/im/oauth2withsso/Oauth2WithSsoApplication.java
@@ -0,0 +1,13 @@
+package com.ab.im.oauth2withsso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Oauth2WithSsoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Oauth2WithSsoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ab/im/oauth2withsso/oauth2withsso/Oauth2WithSsoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ab.im.oauth2withsso.oauth2withsso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Oauth2WithSsoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
